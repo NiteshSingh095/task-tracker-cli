0c84d6f5d399076e6c9a65631178e2341908228d
nitesh095 <anshukumargautam095@gmail.com>
2026-03-22 06:15:05 +0530

    Phase 3 — JSON + Struct Design

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"time"
+	"strconv"
 	// "io/ioutil"
 )
 
@@ -34,8 +37,51 @@ func main() {
 	case "delete":
 		handleDelete()
 
+	case "update":
+		updateTask()
+
 	default:
 		fmt.Println("Unknown Command")
+	}
+}
+
+func timeStamp() string {
+	return time.Now().Format(time.RFC3339);
+}
+
+func updateTask() {
+
+	if len(os.Args) < 4 {
+		fmt.Println("Please provide task ID and new description")
+		return
+	}
+
+	taskID := os.Args[2]
+	// newDescription := os.Args[3]
+
+	if taskID == "" {
+		fmt.Println("Task ID cannot be empty")
+		return
+	}
+
+	data, err := readTasks()
+
+	if err != nil {
+		fmt.Println("Error reading tasks: ", err)
+		return
+	}
+
+	tId, err := strconv.Atoi(taskID)
+
+	if err != nil {
+		fmt.Println("Invalid task ID")
+		return
+	}
+
+	for _, t := range data {
+		if t.ID == int(tId) {
+			fmt.Println("Task found: ", t)
+		}
 	}
 }
 
@@ -48,20 +94,48 @@ func handleAdd() {
 	description := os.Args[2]
 	fmt.Println("Task to add: ", description)
 
-	WriteTasks([]byte(`[{"test": "test task"}]`))
-}
-
-func handleList() {
-	fmt.Println("Handling List Command")
-
-	data, err := readTasks()
+	tasks, err := readTasks()
 
 	if err != nil {
 		fmt.Println("Error reading tasks: ", err)
 		return
 	}
 
-	fmt.Println("Tasks: ", string(data))
+	newTask := Task{
+		ID: getNextTaskId(tasks),
+		Description: description,
+		Status: "TODO",
+		CreatedAt: timeStamp(),
+		UpdatedAt: timeStamp(),
+	}
+
+	tasks = append(tasks, newTask)
+
+	err = WriteTasks(tasks)
+
+	if err != nil {
+		fmt.Println("Error writing tasks: ", err)
+		return
+	}
+
+	fmt.Println("Task added successfully : ", newTask.ID)
+
+}
+
+func handleList() {
+	fmt.Println("Handling List Command")
+
+	task, err := readTasks()
+
+	if err != nil {
+		fmt.Println("Error reading tasks: ", err)
+		return
+	}
+
+	for _, t := range task {
+		fmt.Printf("[%d] %s (%s)\n", t.ID, t.Description, t.Status)
+	}
+
 }
 
 func handleDelete() {
@@ -75,12 +149,38 @@ func handleDelete() {
 	fmt.Println("Task ID to delete: ", taskID)
 }
 
-func readTasks() ([]byte, error) {
-	return os.ReadFile(taskFile)
+func getNextTaskId(tasks []Task) int {
+	if len(tasks) == 0 {
+		return 1
+	}
+
+	return tasks[len(tasks)-1].ID + 1
 }
 
-func WriteTasks(data []byte) error {
-	return os.WriteFile(taskFile, data, 0644)
+func readTasks() ([]Task, error) {
+	data, err := os.ReadFile(taskFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var tasks []Task
+
+	err = json.Unmarshal(data, &tasks)
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func WriteTasks(tasks []Task) error {
+	data, err := json.MarshalIndent(tasks, "", " ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(taskFile, data, 0644);
 }
 
 func ensureTaskFileExists() error {
