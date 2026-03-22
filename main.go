52383b760b46d99266bf1c2ff70cea89c8f2d97c
nitesh095 <anshukumargautam095@gmail.com>
2026-03-22 17:23:22 +0530

    Phase 6 — Smart Listing & Filtering

--- main.go
+++ main.go
@@ -282,10 +282,45 @@ func handleList() {
 		return
 	}
 
+	if len(os.Args) == 2 {
+		printTaskDetails(task)
+		return
+	}
+
+	status := os.Args[2]
+
+	if status == "" {
+		fmt.Println("Status cannot be empty")
+		return
+	}
+
+	if status != statusTodo && status != statusInProgress && status != statusDone {
+		fmt.Println("Invalid status. Allowed values are TODO, IN_PROGRESS, DONE")
+		return
+	}
+
+	var filteredTask []Task
+
+	for _, t := range task {
+		if t.Status == status {
+			filteredTask = append(filteredTask, t)
+		}
+	}
+
+	printTaskDetails(filteredTask)
+
+}
+
+func printTaskDetails(task []Task) {
+
+	if len(task) == 0 {
+		fmt.Println("No tasks found")
+		return
+	}
+
 	for _, t := range task {
 		fmt.Printf("[%d] %s (%s)\n", t.ID, t.Description, t.Status)
 	}
-
 }
 
 func handleDelete() {
